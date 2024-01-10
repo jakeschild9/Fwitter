1596d8f4b48666699c67131c40152e02afbf5679
Jake <jakeschild9@yahoo.com>
2024-01-10 15:54:17 -0600

    Added post object model and added upload function that takes everything from html and saves it into the post database. Had trouble with the post model, all I needed to do was migrate.

--- core/models.py
+++ core/models.py
@@ -1,17 +1,31 @@
+import uuid
+from datetime import datetime
 from django.db import models
 from django.contrib.auth import get_user_model
-
 
 User = get_user_model()
 
 
 # Create your models here.
 class Profile(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE) # unique key for user
-    id_user = models.IntegerField() # users id
-    bio = models.TextField(blank=True) # allows bio to be blank
-    profileimg = models.ImageField(upload_to='profile_images', default='wink.png') # uploading pfp to media folder, and having defalut pfp
-    location = models.CharField(max_length=100, blank=True) # adds max length and allows no location from user
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  # unique key for user
+    id_user = models.IntegerField()  # users id
+    bio = models.TextField(blank=True)  # allows bio to be blank
+    profileimg = models.ImageField(upload_to='profile_images',
+                                   default='wink.png')  # uploading pfp to media folder, and having defalut pfp
+    location = models.CharField(max_length=100, blank=True)  # adds max length and allows no location from user
 
-    def __str__(self): # shows username in admin panel instead of 'objects'
+    def __str__(self):  # shows username in admin panel instead of 'objects'
         return self.user.username
+
+
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    user = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='post_images')
+    caption = models.TextField()
+    created_at = models.DateTimeField(default=datetime.now)
+    no_of_likes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.user
--- core/views.py
+++ core/views.py
@@ -3,21 +3,39 @@ from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from .models import Profile
+from .models import Profile, Post
 
 
 # Create your views here.
 
 @login_required(login_url='signin')
 def index(request):
-    return render(request, 'index.html')
+    user_object = User.objects.get(username=request.user.username)  # getting object of currently logged-in user
+    user_profile = Profile.objects.get(user=user_object)  # getting the profile of the user
+    return render(request, 'index.html', {'user_profile': user_profile})
+
+
+@login_required(login_url='signin')
+def upload(request):
+    if request.method == 'POST':
+        user = request.user.username
+        image = request.FILES.get('image_upload')
+        caption = request.POST['caption']
+
+        new_post = Post.objects.create(user=user, image=image, caption=caption)
+        new_post.save()
+
+        return redirect('/')
+    else:
+        return redirect('/')
+
 
 @login_required(login_url='signin')
 def settings(request):
-    user_profile = Profile.objects.get(user=request.user)
+    user_profile = Profile.objects.get(user=request.user)  # getting profile of currently logged in user
 
     if request.method == "POST":
-        if request.FILES.get('image') == None:
+        if request.FILES.get('image') == None:  # using default image and saving all changes
             image = user_profile.profileimg
             bio = request.POST['bio']
             location = request.POST['location']
@@ -26,7 +44,7 @@ def settings(request):
             user_profile.bio = bio
             user_profile.location = location
             user_profile.save()
-        if request.FILES.get('image') != None:
+        if request.FILES.get('image') != None:  # using new image and saving all changes
             image = request.FILES.get('image')
             bio = request.POST['bio']
             location = request.POST['location']
@@ -36,7 +54,9 @@ def settings(request):
             user_profile.location = location
             user_profile.save()
         return redirect('settings')
-    return render(request, 'setting.html', {'user_profile' : user_profile})
+    return render(request, 'setting.html', {'user_profile': user_profile})
+
+
 def signup(request):
     if request.method == "POST":
         username = request.POST['username']
@@ -55,7 +75,7 @@ def signup(request):
                 user = User.objects.create_user(username=username, email=email, password=password)
                 user.save()
 
-                # log user in and redirect to settings page
+                # log user in
                 user_login = auth.authenticate(username=username, password=password)
                 auth.login(request, user_login)
 
@@ -87,6 +107,7 @@ def signin(request):
             return redirect('signin')
     else:
         return render(request, 'signin.html')
+
 
 @login_required(login_url='signin')
 def logout(request):
